601e9dace5f3f2f294d3329644fe5f9f2df58e1c
Matus Fedorko <mfedorko@atomontage.com>
2021-04-23 09:57:48 +0200

    Add Lua

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include "sol2/include/sol/sol.hpp"
 
 
+
+static void hello_sol()
+{
+	std::cout << "Hello from sol" << std::endl;
+}
 
 int main(void)
 {
 	std::cout << "Hello, World !" << std::endl;
+
+	sol::state lua;
+	lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::coroutine, sol::lib::string, sol::lib::os, sol::lib::math, sol::lib::table, sol::lib::io, sol::lib::debug);
+
+	lua["hello"] = &hello_sol;
+
+	auto res = lua.safe_script(R"(hello())");
+
 	return 0;
 }
